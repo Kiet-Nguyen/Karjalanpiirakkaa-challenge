0dcd4d981e14a0af816f9a3e9aef5c27dc89afd4
Kiet <bunbothomngon@gmail.com>
2019-04-19 14:58:58 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-/* You can write code for solving this challenge here */
+const element = {
+  textArea: document.querySelector('textarea'),
+  result: document.querySelector('.result'),
+}
+
+const sortArr = arr =>
+  arr.sort((a, b) => a['day'] - b['day']);
+
+const calculateDaysMatchIncome = inputData => {
+  // Turn inputData to arr
+  const rawArr = inputData.split(' ');
+
+  // Initialize array of A and C
+  const records = {
+    A: [],
+    C: [],
+  };
+
+  // Push day and income into records
+  rawArr.map((item, index) => {
+    const dayRecord = {};
+
+    if (records.hasOwnProperty(item)) {
+      dayRecord.day = rawArr[index + 1];
+      dayRecord.income = rawArr[index + 2];
+      records[item].push(dayRecord);
+    }
+  });
+
+  // Sort arr A and C
+  sortArr(records.A);
+  sortArr(records.C);
+
+  // Calculate unmatch days
+  const unmatchedDayNumber = records.A.reduce(
+    (accummulator, currentItem, index) => {
+      if (
+        records.C[index] &&
+        currentItem.income !== records.C[index]['income']
+      ) {
+        accummulator += 1;
+      }
+      return accummulator;
+    },
+    0,
+  );
+
+  return unmatchedDayNumber;
+};
+
+element.textArea.addEventListener('keyup', () => {
+  const inputData = element.textArea.value;
+
+  element.result.innerHTML = `
+    The number of days the calculated and actual incomes didn't match is: ${calculateDaysMatchIncome(inputData)} days
+  `;
+})
+
